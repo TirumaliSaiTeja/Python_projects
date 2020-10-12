c3581026e140bfba5601d85378c999f5fdf11f42
TirumaliSaiTeja <tsaiteja7@gmail.com>
2020-10-12 23:52:51 +0530

    added comments

--- chatterbot/app.py
+++ chatterbot/app.py
@@ -1,30 +1,24 @@
 from flask import Flask, render_template, request
 from chatterbot import ChatBot
 from chatterbot.trainers import ChatterBotCorpusTrainer
-
+ 
 app = Flask(__name__)
-
-english_bot = ChatBot('Chatterbot', storage_adapter='chatterbot.storage.SQLStorageAdapter')
+ 
+english_bot = ChatBot("Chatterbot", storage_adapter="chatterbot.storage.SQLStorageAdapter")
 trainer = ChatterBotCorpusTrainer(english_bot)
-trainer.train('chatterbot.corpus.english')
+trainer.train("chatterbot.corpus.english")
 
-
-@app.route('/')
+// goes to root of the site
+ 
+@app.route("/")
 def home():
-    return render_template('index.html')
-
-@app.route('/get')
+    return render_template("index.html")
+ 
+@app.route("/get")
 def get_bot_response():
     userText = request.args.get('msg')
     return str(english_bot.get_response(userText))
-
-if __name__ == '__main__':
+ 
+ 
+if __name__ == "__main__":
     app.run()
-
-
-
-
-
-
-
-
